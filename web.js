81721a367403d18b2f255941b11bba797062381e
Danycg <ubuntu@ip-172-31-0-228.us-west-2.compute.internal>
2013-07-09 15:45:47 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+var msg = fs.readFile('./index.html', function (err, data) {
+    if (err) throw err;
+});
+
+
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
